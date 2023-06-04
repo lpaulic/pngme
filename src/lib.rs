91f40972fd9fd5cd1cf10eba8ad5e0513d22391e
Luka Paulić <luka.paulic0@gmail.com>
2023-06-04 14:06:44 +0200

    feat: add command line argument handling
    Add clap library as dependency
    Add tmp folder for test png assets to .gitignore
    Update error handling in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,62 @@
-use std::error::Error;
-
-// TODO: check should use be first or mod
 mod args;
 mod chunk;
 mod chunk_type;
 mod commands;
 mod png;
 
-pub struct Config {
-    pub message: String,
-    pub png_file: String,
+use args::PngMeArgs;
+use clap::Parser;
+
+#[derive(Debug)]
+pub enum ConfigError {
+    UnsupportedArgument,
+    ArgumentParsing(clap::Error),
 }
 
-impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next(); // skipping program name on first position
-
-        let message = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a message to embed in PNG"),
-        };
-
-        let png_file = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a PNG file path"),
-        };
-
-        Ok(Config { message, png_file })
+impl From<clap::Error> for ConfigError {
+    fn from(item: clap::Error) -> ConfigError {
+        ConfigError::ArgumentParsing(item)
     }
 }
 
-pub fn run(_config: Config) -> Result<(), Box<dyn Error>> {
-    todo!()
+pub struct Config {
+    args: PngMeArgs,
+}
+
+impl Config {
+    pub fn build(args: impl Iterator<Item = String>) -> Result<Config, ConfigError> {
+        Ok(Config {
+            args: PngMeArgs::try_parse_from(args)?,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), ConfigError> {
+    match config.args {
+        PngMeArgs::Encode(encode) => {
+            println!("File path: {}", encode.file_path.display());
+            println!("Chunk type: {}", encode.chunk_type);
+            println!("Message: {}", encode.message);
+            println!(
+                "Output file path: {}",
+                match encode.output_file_path {
+                    Some(p) => p.display().to_string(),
+                    None => "n/a".to_string(),
+                }
+            );
+        }
+        PngMeArgs::Decode(decode) => {
+            println!("File path: {}", decode.file_path.display());
+            println!("Chunk type: {}", decode.chunk_type);
+        }
+        PngMeArgs::Remove(remove) => {
+            println!("File path: {}", remove.file_path.display());
+            println!("Chunk type: {}", remove.chunk_type);
+        }
+        PngMeArgs::Print(print) => {
+            println!("File path: {}", print.file_path.display());
+        }
+    };
+
+    Ok(())
 }
